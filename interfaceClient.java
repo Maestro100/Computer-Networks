70af1856c5802c27b8b54118e31fb112e29b5f2d
Ayush Chaurasia <ayushchaurasia2879@gmail.com>
2019-08-31 12:13:49 +0530

    git init

--- interfaceClient.java
+++ interfaceClient.java
@@ -11,12 +11,10 @@ class TCPClient {
 
         Socket clientSocket = new Socket("localhost", 6789); 
 
-        DataOutputStream outToServer = 
-          new DataOutputStream(clientSocket.getOutputStream()); 
+        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream()); 
 
         
-        BufferedReader inFromServer = 
-          new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); 
+        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); 
 
         while(true) {
 
@@ -31,6 +29,14 @@ class TCPClient {
         }
 
 //        clientSocket.close(); 
+//hsnd aj dmnasbdhas dnasbldna sdj asld asd
+
+
+//git status
+//git pull
+//git add .
+//git commit -m "messagefsjdbf"
+//git push
                    
     } 
 } 
