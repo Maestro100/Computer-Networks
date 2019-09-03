db597e75b4849589301289509508107952db84b3
Ayush Chaurasia <ayushchaurasia2879@gmail.com>
2019-09-03 22:02:26 +0530

    making copies before enc-dcr.

--- testC.java
+++ testC.java
@@ -1,28 +1,28 @@
-import java.io.*; 
-import java.net.*; 
-class Client { 
+// import java.io.*; 
+// import java.net.*; 
+// class Client { 
 
-    public static void main(String argv[]) throws Exception { 
+//     public static void main(String argv[]) throws Exception { 
         
-        String sentence; 
-        String modifiedSentence; 
+//         String sentence; 
+//         String modifiedSentence; 
 
-        DataInputStream inFromUser = 
-          new DataInputStream(System.in); 
+//         DataInputStream inFromUser = 
+//           new DataInputStream(System.in); 
 
-        Socket clientSocket = new Socket("localhost", 6789); 
+//         Socket clientSocket = new Socket("localhost", 6789); 
 
-        DataOutputStream outToServer = 
-          new DataOutputStream(clientSocket.getOutputStream()); 
+//         DataOutputStream outToServer = 
+//           new DataOutputStream(clientSocket.getOutputStream()); 
        
-        DataInputStream inFromServer = 
-          new DataInputStream(clientSocket.getInputStream()); 
+//         DataInputStream inFromServer = 
+//           new DataInputStream(clientSocket.getInputStream()); 
 
-        while(true){
-             sentence = inFromUser.readLine(); 
-             outToServer.writeUTF(sentence + '\n'); 
-             modifiedSentence = inFromServer.readUTF(); 
-             System.out.println("FROM SERVER: " + modifiedSentence); 
-        }          
-    } 
-} 
+//         while(true){
+//              sentence = inFromUser.readLine(); 
+//              outToServer.writeUTF(sentence + '\n'); 
+//              modifiedSentence = inFromServer.readUTF(); 
+//              System.out.println("FROM SERVER: " + modifiedSentence); 
+//         }          
+//     } 
+// } 
--- testS.java
+++ testS.java
@@ -1,52 +1,52 @@
-import java.io.*; 
-import java.net.*; 
+// import java.io.*; 
+// import java.net.*; 
 
-class Server { 
-  public static void main(String argv[]) throws Exception{ 
-        ServerSocket welcomeSocket = new ServerSocket(6789); 
+// class Server { 
+//   public static void main(String argv[]) throws Exception{ 
+//         ServerSocket welcomeSocket = new ServerSocket(6789); 
 
-        while(true) { 
-            Socket connectionSocket = welcomeSocket.accept(); 
-            DataInputStream inFromClient = 
-            new DataInputStream(connectionSocket.getInputStream()); 
+//         while(true) { 
+//             Socket connectionSocket = welcomeSocket.accept(); 
+//             DataInputStream inFromClient = 
+//             new DataInputStream(connectionSocket.getInputStream()); 
 
-            DataOutputStream outToClient = 
-            new DataOutputStream(connectionSocket.getOutputStream()); 
+//             DataOutputStream outToClient = 
+//             new DataOutputStream(connectionSocket.getOutputStream()); 
 
-            SocketThread socketThread = new SocketThread(connectionSocket, inFromClient, outToClient);
-            Thread thread = new Thread(socketThread);
-            thread.start();  
-        }
-    } 
-} 
+//             SocketThread socketThread = new SocketThread(connectionSocket, inFromClient, outToClient);
+//             Thread thread = new Thread(socketThread);
+//             thread.start();  
+//         }
+//     } 
+// } 
  
 
-class SocketThread implements Runnable {
+// class SocketThread implements Runnable {
 
-    String clientSentence; 
-    String capitalizedSentence; 
-    Socket connectionSocket;
-    DataInputStream inFromClient;
-    DataOutputStream outToClient;
+//     String clientSentence; 
+//     String capitalizedSentence; 
+//     Socket connectionSocket;
+//     DataInputStream inFromClient;
+//     DataOutputStream outToClient;
    
-    SocketThread (Socket connectionSocket, DataInputStream inFromClient, DataOutputStream outToClient) {
-	    this.connectionSocket = connectionSocket;
-        this.inFromClient = inFromClient;
-        this.outToClient = outToClient;
-    } 
+//     SocketThread (Socket connectionSocket, DataInputStream inFromClient, DataOutputStream outToClient) {
+// 	    this.connectionSocket = connectionSocket;
+//         this.inFromClient = inFromClient;
+//         this.outToClient = outToClient;
+//     } 
 
-    public void run() {
-        while(true) { 
-            try {
-                clientSentence = inFromClient.readLine(); 
+//     public void run() {
+//         while(true) { 
+//             try {
+//                 clientSentence = inFromClient.readLine(); 
 
-                System.out.println(clientSentence);
-                outToClient.writeUTF(clientSentence.substring(3));
-                System.out.println("here");     
-            } 
-            catch(Exception e) {
-                System.out.println(e);
-            }
-        } 
-    }
-}
+//                 System.out.println(clientSentence);
+//                 outToClient.writeUTF(clientSentence.substring(3));
+//                 System.out.println("here");     
+//             } 
+//             catch(Exception e) {
+//                 System.out.println(e);
+//             }
+//         } 
+//     }
+// }
